c9cfbfd57b3397d1bd25180fe6244be015641cb2
aldiindrawan <45248095+aldiindrawan@users.noreply.github.com>
2020-06-09 09:43:17 +0700

    develope login

---
+++ application/views/login/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url()?>/assets/css/login.css">
+    <title>Admin Login</title>
+</head>
+<body>
+    <div class="login-form-card">
+        <div class="login-form-container">
+            <span id="login-title">
+                Selamat Datang Di
+            </span>
+        <div class="login-logo">
+            <img src="" alt="Bank Soal dan Materi Itera"> 
+        </div>
+
+        <div class="description">
+            Institut Teknologi Sumatera <br> Lampung Selatan
+        </div>
+
+            <form method="post" action="">
+                <input type="text" name="username" id="email-admin" placeholder="Email">
+                <input type="password" name="password" id="password-admin" placeholder="Password">
+                <input type="submit" value="Login">
+            </form>
+        </div>
+
+        <div class=foot>
+            &copy 2020 IniTeam x KM-ITERA  
+        </div>
+
+        
+
+    </div>
+
+    
+</body>
+    
+</html>

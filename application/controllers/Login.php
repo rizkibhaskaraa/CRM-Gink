9d1729943dd48cf4e6507a0908275cf372a2cb09
muttaqin123 <muttaqin0708@gmail.com>
2020-06-09 16:35:07 +0700

    Add Register
    Add Register

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,8 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
-	public function __construct(){
+class Login extends CI_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
 		$this->load->model('login_model');
 		$this->load->library('form_validation');
@@ -13,7 +15,8 @@ class Login extends CI_Controller {
 		$this->load->view('login/login');
 	}
 
-	public function login(){
+	public function login()
+	{
 		$this->form_validation->set_rules('username', 'Username', 'required');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 		if ($this->form_validation->run() == false) {
@@ -23,15 +26,16 @@ class Login extends CI_Controller {
 		}
 	}
 
-	public function validationLogin(){
+	public function validationLogin()
+	{
 		$email = $this->input->post('username');
 		$password = $this->input->post('password');
 		$staff = $this->login_model->getstaff($email);
 		$data["staff"] = $staff;
 		if ($staff) { //jika hasil ada
 			$saved_password = password_hash($staff['password'], PASSWORD_DEFAULT);
-			if(password_verify($password, $saved_password)){
-				$this->load->view('Home/home',$data); //jgnn lupa diubah tujuan
+			if (password_verify($password, $saved_password)) {
+				$this->load->view('Home/home', $data); //jgnn lupa diubah tujuan
 			} else {
 				$this->load->view('login/login');
 			}
@@ -41,7 +45,8 @@ class Login extends CI_Controller {
 		}
 	}
 
-	public function register(){
-		$this->load->view('login/register');
-	}
+	// public function register()
+	// {
+	// 	$this->load->view('login/register');
+	// }
 }

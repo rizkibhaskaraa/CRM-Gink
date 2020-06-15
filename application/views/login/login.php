f083cd4ce2bdc344c0a6eee2dca3617d024f076d
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-06-15 11:16:31 +0700

    login page update

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -51,40 +51,34 @@
                     <div class="col-md-8 col-lg-6 col-xl-4">
                         <!-- Sign In Block -->
                         <div class="block block-themed block-fx-shadow mb-0">
-                            <div class="block-header">
-                                <h3 class="block-title">Sign In</h3>
-                                <div class="block-options">
-                                    <a class="btn-block-option font-size-sm" href="op_auth_reminder.html">Forgot Password?</a>
-                                    <a class="btn-block-option" href="op_auth_signup.html" data-toggle="tooltip" data-placement="left" title="New Account">
-                                        <i class="fa fa-user-plus"></i>
-                                    </a>
-                                </div>
-                            </div>
+                                                      
+                            
                             <div class="block-content">
-                                <div class="p-sm-3 px-lg-4 py-lg-5">
-                                    <h1 class="mb-2">OneUI</h1>
-                                    <p>Welcome, please login.</p>
+                            <img class="img-fluid mt-3" src="<?php echo base_url('assets/img/logo.png')?> " alt="Gink Technology">
+                            
+                            
+                                <div class=" px-lg-4 py-lg-5 " >
 
+                                
+                                    
+                                    <h1 class="mb-2 ">Sign In</h1>
+                                    <p>Masuk untuk melanjutkan akses.</p>
+                                
                                     <!-- Sign In Form -->
                                     <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _es6/pages/op_auth_signin.js) -->
                                     <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
                                     <form class="js-validation-signin" action="be_pages_auth_all.html" method="POST">
-                                        <div class="py-3">
+                                        <div class="py-1">
                                             <div class="form-group">
                                                 <input type="text" class="form-control form-control-alt form-control-lg" id="login-username" name="login-username" placeholder="Username">
                                             </div>
                                             <div class="form-group">
                                                 <input type="password" class="form-control form-control-alt form-control-lg" id="login-password" name="login-password" placeholder="Password">
                                             </div>
-                                            <div class="form-group">
-                                                <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input" id="login-remember" name="login-remember">
-                                                    <label class="custom-control-label font-w400" for="login-remember">Remember Me</label>
-                                                </div>
-                                            </div>
+                                          
                                         </div>
                                         <div class="form-group row">
-                                            <div class="col-md-6 col-xl-5">
+                                            <div class="container-fluid">
                                                 <button type="submit" class="btn btn-block btn-primary">
                                                     <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
                                                 </button>
@@ -100,7 +94,7 @@
                 </div>
             </div>
             <div class="content content-full font-size-sm text-muted text-center">
-                <strong>OneUI 4.2</strong> &copy; <span data-toggle="year-copy"></span>
+                <strong>ARM TEAM</strong> &copy; <span data-toggle="year-copy"></span>
             </div>
         </div>
     </div>

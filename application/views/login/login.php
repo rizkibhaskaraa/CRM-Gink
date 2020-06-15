470b56b84f03532ded3fe25282f7d260de73f694
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-06-15 09:42:55 +0700

    add all front end needs

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -2,30 +2,150 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/login/login.css')?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/body.css')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
     <title>Staff Login</title>
+
+        <meta name="description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
+
+        <!-- Open Graph Meta -->
+        <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">
+        <meta property="og:site_name" content="OneUI">
+        <meta property="og:description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+
+
+
+
+        <!-- Icons -->
+        <link rel="shortcut icon" href="assets/oneui/media/favicons/favicon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="assets/oneui/media/favicons/favicon-192x192.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/oneui/media/favicons/apple-touch-icon-180x180.png">
+        <!-- END Icons -->
+
+        <!-- Stylesheets -->
+        <!-- Fonts and OneUI framework -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
+        <link rel="stylesheet" id="css-main" href="assets/oneui/css/oneui.min.css">
+
+
+
 </head>
 <body>
-    <div class="login-form-card">
-        <div class="login-form-container">
-            <span id="login-title">
-                Selamat Datang Di
-            </span>
-        <div class="login-logo">
-            <img src="<?php echo base_url('assets/img/logo.png')?>" alt="Gink Technology"> 
-        </div>
-            <form method="post" action="<?php echo base_url('index.php/login/login')?>">
-                <input type="text" name="username" id="email-admin" placeholder="Email / Username">
-                <input type="password" name="password" id="password-admin" placeholder="Password">
-                <input type="submit" value="Login">
-            </form>
-            <div class="register">
-                <a class="reg" href="<?php echo base_url('index.php/login/register')?>">Register</a>
-                <a class="forget-pw"  href="<?php echo base_url('index.php/login/email')?>">Lupa Password ?</a>
+
+
+<div id="page-container">
+
+<!-- Main Container -->
+<main id="main-container">
+
+    <!-- Page Content -->
+    <div class="bg-image" style="background-image: url('assets/oneui/media/photos/photo6@2x.jpg');">
+        <div class="hero-static bg-white-95">
+            <div class="content">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-4">
+                        <!-- Sign In Block -->
+                        <div class="block block-themed block-fx-shadow mb-0">
+                            <div class="block-header">
+                                <h3 class="block-title">Sign In</h3>
+                                <div class="block-options">
+                                    <a class="btn-block-option font-size-sm" href="op_auth_reminder.html">Forgot Password?</a>
+                                    <a class="btn-block-option" href="op_auth_signup.html" data-toggle="tooltip" data-placement="left" title="New Account">
+                                        <i class="fa fa-user-plus"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="block-content">
+                                <div class="p-sm-3 px-lg-4 py-lg-5">
+                                    <h1 class="mb-2">OneUI</h1>
+                                    <p>Welcome, please login.</p>
+
+                                    <!-- Sign In Form -->
+                                    <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.min.js which was auto compiled from _es6/pages/op_auth_signin.js) -->
+                                    <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                                    <form class="js-validation-signin" action="be_pages_auth_all.html" method="POST">
+                                        <div class="py-3">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control form-control-alt form-control-lg" id="login-username" name="login-username" placeholder="Username">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-alt form-control-lg" id="login-password" name="login-password" placeholder="Password">
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input" id="login-remember" name="login-remember">
+                                                    <label class="custom-control-label font-w400" for="login-remember">Remember Me</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-md-6 col-xl-5">
+                                                <button type="submit" class="btn btn-block btn-primary">
+                                                    <i class="fa fa-fw fa-sign-in-alt mr-1"></i> Sign In
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!-- END Sign In Form -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END Sign In Block -->
+                    </div>
+                </div>
+            </div>
+            <div class="content content-full font-size-sm text-muted text-center">
+                <strong>OneUI 4.2</strong> &copy; <span data-toggle="year-copy"></span>
             </div>
         </div>
     </div>
+    <!-- END Page Content -->
+
+</main>
+<!-- END Main Container -->
+</div>
+<!-- END Page Container -->
+
+<!--
+OneUI JS Core
+
+Vital libraries and plugins used in all pages. You can choose to not include this file if you would like
+to handle those dependencies through webpack. Please check out assets/_es6/main/bootstrap.js for more info.
+
+If you like, you could also include them separately directly from the assets/js/core folder in the following
+order. That can come in handy if you would like to include a few of them (eg jQuery) from a CDN.
+
+assets/js/core/jquery.min.js
+assets/js/core/bootstrap.bundle.min.js
+assets/js/core/simplebar.min.js
+assets/js/core/jquery-scrollLock.min.js
+assets/js/core/jquery.appear.min.js
+assets/js/core/js.cookie.min.js
+-->
+<script src="assets/oneui/js/oneui.core.min.js"></script>
+
+<!--
+OneUI JS
+
+Custom functionality including Blocks/Layout API as well as other vital and optional helpers
+webpack is putting everything together at assets/_es6/main/app.js
+-->
+<script src="assets/oneui/js/oneui.app.min.js"></script>
+
+<!-- Page JS Plugins -->
+<script src="assets/oneui/js/plugins/jquery-validation/jquery.validate.min.js"></script>
+
+<!-- Page JS Code -->
+<script src="assets/oneui/js/pages/op_auth_signin.min.js"></script>y
+
+
+
+
+
 </body>
 </html>

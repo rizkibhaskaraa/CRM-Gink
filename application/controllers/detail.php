df2f516c32383e69edcaa326197877e21e15550b
muttaqin123 <muttaqin0708@gmail.com>
2020-06-30 13:30:54 +0700

    Add komentar/dokumentasi program
    Komentar/dokumentasi program pada controller detail.php

--- application/controllers/detail.php
+++ application/controllers/detail.php
@@ -23,41 +23,41 @@ class Detail extends CI_Controller
         $data["employ_dept"] = $employ["id_departemen"];
         $data["status"] = $employ["status_employ"];
         //akhir data employ yang akses
+
         $data["cekTabel"] = $cekTabel; //cek table asal
-
         $data["getPJ"] = $this->detail_model->getsemuaPJ($employ["id_departemen"]); //data employ di departemen
-
         $data["task"] = $this->detail_model->getdetail($task); //data task dengan id $task
         $data["PJ_task"] = $this->detail_model->getPJ_task($task); //data task dengan id $task
-
         $data["nama_kirim"] = $this->detail_model->getnama_kirim($task); //nama pengirim
-
         $data["dept_PJtask"] = $this->detail_model->getdeptPJTask($task); //data departemen PJ task
-        $data["komentar"] = $this->detail_model->getkomentar($task);
-        $data["komentarsub"] = $this->detail_model->getkomentarsub($task);
+        $data["komentar"] = $this->detail_model->getkomentar($task); //ambil data komentar, untuk task parent
+        $data["komentarsub"] = $this->detail_model->getkomentarsub($task); //ambil data komentar, untuk subtask
         $data["nama_dept"] = $this->detail_model->getdeptcalonpj($data["employ_dept"]); //data departemen calon PJ task
         $data['namaPJ'] = $this->detail_model->getnama_PJ($task); //data nama PJ task
         $data['tugas_employ'] = $this->detail_model->gettugaspj($data["nama_dept"]); //data tugas milik calon PJ
-        $data["semua_employ"] = $this->detail_model->getsemuaemploy($data["employ_dept"]);
-        $data["subtask"] = $this->detail_model->getsubtask($task);
-        $data["subtaskselesai"] = $this->detail_model->getsubtaskselesai($task);
+        $data["semua_employ"] = $this->detail_model->getsemuaemploy($data["employ_dept"]); //data employ pada suatu departemen
+        $data["subtask"] = $this->detail_model->getsubtask($task); //ambil subtask berdasarkan parent task
+        $data["subtaskselesai"] = $this->detail_model->getsubtaskselesai($task); //get subtask yang sudah selesai
         $this->load->view('detail/detail', $data);
     }
+    //add PJ task
     public function ubahPJ($id, $task)
     {
         $ubah = $this->detail_model->ubahPJ($this->input->post("PJbaru"), $task);
         $user = $this->detail_model->getuser($id);
         redirect(base_url('index.php/home/index/') . $user['username']);
     }
+    //fungsi ubah status task menjadi selesai
     public function ubahstatustask($id, $task)
     {
         $ubah = $this->detail_model->ubahstatustask($task);
         redirect(base_url('index.php/home/detail/') . $id . "/" . $task . "/Request");
     }
+    //add fungsi tambah laporan
     public function insertLaporan($id, $task)
     {
-        $config['upload_path']          = './upload/';
-        $config['allowed_types']        = 'gif|jpg|png|img|jpeg|doc|docx|xls|xlsx|ppt|pptx|pdf';
+        $config['upload_path']          = './upload/'; //letak folder file yang akan diupload
+        $config['allowed_types']        = 'gif|jpg|png|img|jpeg|doc|docx|xls|xlsx|ppt|pptx|pdf'; //jenis file yang dapat diterima
 
         $this->load->library('upload', $config);
         date_default_timezone_set('Asia/Bangkok');
@@ -73,7 +73,7 @@ class Detail extends CI_Controller
         $user = $this->detail_model->getuser($id);
         redirect(base_url('index.php/home/index/') . $user['username']);
     }
-
+    //menambahkan subtiket
     public function addsubtiket($id_employ, $id_Task, $tabel)
     {
         date_default_timezone_set('Asia/Bangkok');
@@ -96,6 +96,7 @@ class Detail extends CI_Controller
         $this->detail_model->insert_sub_task($data_sub_task, $id_employ, $id_Task);
         redirect(base_url('index.php/detail/detailumum/') . $id_employ . "/" . $id_Task . "/" . $tabel);
     }
+    //add fungsi tambah komentar
     public function addkomen($idtask, $nama, $id_employ, $tabel, $dept)
     {
         date_default_timezone_set('Asia/Bangkok');
@@ -112,6 +113,7 @@ class Detail extends CI_Controller
         $this->detail_model->buatkomen($data);
         redirect(base_url('index.php/detail/detailumum/') . $id_employ . "/" . $idtask . "/" . $tabel);
     }
+    //add fungsi delete komentar
     public function deletekomen($idtask, $id_employ, $tabel, $idkomen)
     {
         $this->detail_model->deletekomen($idkomen);

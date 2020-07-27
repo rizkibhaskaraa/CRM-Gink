9a4e570e44a52ae11c64a4f2fea1a22b295fb307
redrexaye <45308379+rizkibhaskara@users.noreply.github.com>
2020-07-27 23:04:22 +0700

    Clean code and change icon & title

--- application/views/Home/home_ceo.php
+++ application/views/Home/home_ceo.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Dashboard </title>
+    <title>Dashboard - Task Manager</title>
     <!-- Icons -->
     <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
     <link rel="shortcut icon" href="<?php echo base_url('assets/oneui/media/favicons/favicon.png') ?>">
     <link rel="icon" type="image/png" sizes="192x192" href="<?php echo base_url('assets/oneui/media/favicons/favicon-192x192.png') ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('assets/oneui/media/favicons/apple-touch-icon-180x180.png') ?>">
+    
     <!-- END Icons -->
     <!-- Stylesheets -->
     <!-- Fonts and OneUI framework -->
